be03469d8660bba60ebef59eb013b4e598a19e0e
José Ramírez Cantú <arcantu97@gmail.com>
2024-06-29 22:01:15 -0600

    Melos integration

---
+++ apps/linkaform_access_control/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.linkaform_access_control
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps/linkaform_access_control/assets/l10n/app_localizations_es.dart
@@ -0,0 +1,12 @@
+import 'app_localizations.dart';
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get linkaform_title => 'Linkaform';
+
+  @override
+  String get access_control_title => 'Control de acceso ';
+}
